fe1e1ba7d6376ef8e2c644a6e644d7768065014d
Amanch Esmailzadeh <5677914+manzdeh@users.noreply.github.com>
2024-11-04 22:16:41 +0100

    debug/release selection for linux build

--- build.sh
+++ build.sh
@@ -4,16 +4,22 @@ scriptpath=$(realpath "$0")
 
 pushd $(dirname "$scriptpath") > /dev/null
 
-echo Compiling DEBUG build...
-
 compiler_flags="-std=c++20 -Werror -Wall -Wunused-parameter \
     -Wno-gnu-zero-variadic-macro-arguments -Wno-gnu-anonymous-struct -Wno-nested-anon-types \
-    -O0 -g -pedantic \
-    -m64 -masm=intel -mrdrnd"
+    -pedantic -m64 -masm=intel -mrdrnd"
 defines="-D_POSIX_C_SOURCE=200809L -DAE_PLATFORM_LINUX"
 libs="-lstdc++ -lc -lm"
 
 translation_units=$(find . -name "*.cpp" ! -name "*win32.cpp")
+
+if [[ "$*" == *"--release"* ]]
+    then
+        echo Compiling RELEASE build...
+        compiler_flags="$compiler_flags -O2"
+    else
+        echo Compiling DEBUG build...
+        compiler_flags="$compiler_flags -O0 -g"
+fi
 
 clang++ $compiler_flags \
     $defines \
